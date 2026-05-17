bbf4d2b4752abe1c9f5dd0194b230d735b38665c
Trifun Derenj <trifun.derenj@gmail.com>
2026-05-17 16:50:06 +0200

    [INIT] spring boot

---
+++ oblak/src/main/java/com/tim8/oblak/OblakApplication.java
@@ -0,0 +1,13 @@
+package com.tim8.oblak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OblakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OblakApplication.class, args);
+	}
+
+}
---
+++ oblak/src/test/java/com/tim8/oblak/OblakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim8.oblak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OblakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
