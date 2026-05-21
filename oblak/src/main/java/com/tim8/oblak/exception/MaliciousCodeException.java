3cb2df07b6c84d7d3c004133f539da9f13cea151
Trixer <164249760+TrifunDerenj@users.noreply.github.com>
2026-05-21 06:11:36 +0200

    [ADD] MaliciousCodeException

---
+++ oblak/src/main/java/com/tim8/oblak/exception/MaliciousCodeException.java
@@ -0,0 +1,8 @@
+package com.tim8.oblak.exception;
+
+public class MaliciousCodeException extends RuntimeException {
+
+    public MaliciousCodeException(String message) {
+        super(message);
+    }
+}
